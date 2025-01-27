07fc70adb79bffc55acd9ee70b0c0cf47a8d5989
n-fukukawa <fukulab11@icloud.com>
2025-01-28 08:43:36 +0900

    SwiftUI tutorials; Chapter4

---
+++ Landmarks/Views/Pageview/PageView.swift
@@ -0,0 +1,28 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Naruki Fukukawa on 2025/01/28.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 1
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+                .padding(.bottom, 8)
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0 )})
+}
